7c39fb94713a63aa630c03600b780afe54a2880e
AndreyErin <eaa.forwork@gmail.com>
2024-10-28 17:00:12 +0700

    Вкласс Config добавлены поля онисывающие настрйоки для записи клиентов

--- HairSalon/Model/Configuration/Config.cs
+++ HairSalon/Model/Configuration/Config.cs
@@ -7,5 +7,12 @@
 
         //флаг включения/выключения отображения ссылки на скачку мобильного приложения
         public bool MobileAppEnabled {  get; set; } = false ;
+
+        //параметры записи
+        public bool RecordEnable { get; set; } = false ;
+        public int StartTimeOfDaty { get; set; }//начало рабочего дня
+        public int EndTimeOfDaty { get;set; }//конец рабочего дня
+        public int NumberOfDaysForRecords { get; set; }//количество доступных дней для записи
+        public WorkDaysOfWeek  WorkDaysOfWeek { get; set; }//рабочие дни недели
     }
 }
