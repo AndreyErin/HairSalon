03c7d889b9d941bb938f7edde825d3c548be01df
AndreyErin <eaa.forwork@gmail.com>
2024-11-04 16:48:08 +0700

    Добавлены AdminController, ConfigurationController, ConfigurationViewComponent и views к ним

---
+++ HairSalon/Components/ConfigurationViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using HairSalon.Model.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace HairSalon.Components
+{
+    public class ConfigurationViewComponent: ViewComponent
+    {
+        private IRepositoryOfConfiguration _repositoryOfConfiguration;
+
+        public ConfigurationViewComponent(IRepositoryOfConfiguration repositoryOfConfiguration)
+        {
+            _repositoryOfConfiguration = repositoryOfConfiguration;
+        }
+        public ViewViewComponentResult Invoke() 
+        {
+            return View(_repositoryOfConfiguration.GetConfig()); 
+        }
+    }
+}
---
+++ HairSalon/Controllers/ConfigurationController.cs
@@ -0,0 +1,19 @@
+﻿using HairSalon.Model.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HairSalon.Controllers
+{
+    public class ConfigurationController: Controller
+    {
+        IRepositoryOfConfiguration _repositoryOfConfiguration;
+        public ConfigurationController(IRepositoryOfConfiguration repositoryOfConfiguration)
+        {
+            _repositoryOfConfiguration = repositoryOfConfiguration;
+        }
+
+        public ViewResult Index() 
+        {
+            return View(_repositoryOfConfiguration.GetConfig());
+        }
+    }
+}
