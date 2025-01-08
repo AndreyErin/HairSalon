5ebf0d49c318731fdd8608ec538f90c02f1d70e7
AndreyErin <eaa.forwork@gmail.com>
2025-01-08 17:16:55 +0700

    В базу добавлен администратор. В AccountController написаны методы входа и выхода с  соответсвующими предаставлениями

--- HairSalon/Controllers/Admin/AccountController.cs
+++ HairSalon/Controllers/Admin/AccountController.cs
@@ -1,12 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HairSalon.Model.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HairSalon.Controllers.Admin
 {
     public class AccountController : Controller
     {
-        public IActionResult Login()
+        private SignInManager<User> _signInManager;
+        public AccountController(SignInManager<User> signInManager)
         {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string ReturnUrl)
+        {
+            if (ReturnUrl != null) 
+            {
+                //если в запросе есть url возврата
+                TempData["ReturnUrl"] = ReturnUrl;
+            }
+            else
+            {
+                //если это не первая поптытка входа и url из запроса уже потерялся,
+                //то пересохраняем его
+                TempData.Keep();
+            }
+
             return View();
         }
+
+        //Admin - Admin123$
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string name, string password)
+        {
+            var ReturnUrl = TempData["ReturnUrl"] as string;
+
+            var resutl = await _signInManager.PasswordSignInAsync(name, password,false,false);
+            if (resutl.Succeeded) 
+            {
+                return Redirect(ReturnUrl ?? "/Admin");
+            }
+            else
+            {
+                TempData.Keep();
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Login", "Account");
+        }
+
     }
 }
